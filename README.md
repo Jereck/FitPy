4c97224372bcfc18ae83bfd0025834b10db54f19
Jake <jakereck@gmail.com>
2017-07-01 13:53:45 -0700

    More Functionality
    Added some more functionality to the Person class and cleaned it up

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FitPy
+
+#This is a program I am writing and creating as I learn more and more about Python.
+#The hopes is to watch this program evolve from something simple to a more complex and robust program
+
+############### STAGE ONE ##################################
+#The program is at it's simplest form. It should take in a users name, age, heigh, weight, activity level, and goals
+#Based on the information that the user inputs, it should output their TDEE (Total Daily Energy Expenditure) and a sample meal plan
+
+
+############### STAGE TWO ##################################
--- fit.py
+++ fit.py
@@ -1,7 +1,9 @@
 #CREATE THE PERSON CLASS
 class Person:
-	def __init__(self, age, weight, height, activity_level, goal, **kwargs):
+	def __init__(self, name, age, sex, weight, height, activity_level, goal, **kwargs):
+      self.name = ""
       self.age = 0
+      self.sex = ""
       self.weight = 0
       self.height = 0
       self.activity_level = 0
@@ -12,10 +14,17 @@ class Person:
 
    def total_daily_expend(self):
       #INSERT FORMULA FOR TDEE HERE
+      #OUTPUT BASED ON MALE OR FEMALE
+      #WOMAN
+      return 655 + (9.6 * self.weight) + (1.8 * self.height) - (4.7 * self.age)
+      #MALE
+      return 66 + (13.7 * self.weight) + (5 * self.height) - (6.8 * self.age)
 
-#CREATE THE FUNCTIONS
+
+#CREATE THE FUNCTIONS FOR GAIN, MAINTAIN, LOSE
 def gain():
-
+   return gain_tdee
 def maintain():
-
+   return maintain_tdee
 def lose():
+   return lose_tdee

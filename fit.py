287eb32d8627344d2c4eaeab63901ac39a1592da
Jake <jakereck@gmail.com>
2017-07-01 11:50:07 -0700

    Initial Upload
    Initial Upload of FitPy Project to view as a starting point

---
+++ fit.py
@@ -0,0 +1,16 @@
+#CREATE THE PERSON CLASS
+class Person:
+	age = 0
+   weight = 0
+   height = 0
+   activity_level = 0
+   goal = ""
+   def total_daily_expend(self):
+      #INSERT FORMULA FOR TDEE HERE
+
+#CREATE THE FUNCTIONS
+def gain():
+
+def maintain():
+
+def lose():

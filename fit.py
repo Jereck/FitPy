18d8a0d3ca802f0202a615455e06e8d71396cb03
Jake <jakereck@gmail.com>
2017-07-01 15:59:51 -0700

    More Changes
    Refactoring and making the code more readable

--- fit.py
+++ fit.py
@@ -1,13 +1,13 @@
 #CREATE THE PERSON CLASS
 class Person:
    def __init__(self, name, age, sex, weight, height, activity_level, goal, **kwargs):
-      self.name = ""
-      self.age = 0
-      self.sex = ""
-      self.weight = 0
-      self.height = 0
-      self.activity_level = 0
-      self.goal = ""
+      self.name = name
+      self.age = age
+      self.sex = sex
+      self.weight = weight
+      self.height = height
+      self.activity_level = activity_level
+      self.goal = goal
 
       for key, value in kwargs.items():
          setattr(self, key, value)
@@ -16,9 +16,11 @@ class Person:
       #INSERT FORMULA FOR BMR HERE
       #OUTPUT BASED ON MALE OR FEMALE
       #WOMAN
-      return 655 + (9.6 * self.weight) + (1.8 * self.height) - (4.7 * self.age)
+      if sex == 'f'
+         return 655 + (9.6 * self.weight) + (1.8 * self.height) - (4.7 * self.age)
       #MALE
-      return 66 + (13.7 * self.weight) + (5 * self.height) - (6.8 * self.age)
+      if sex == 'm'
+         return 66 + (13.7 * self.weight) + (5 * self.height) - (6.8 * self.age)
 
    def total_daily_expend(self):
       #INSERT FORMULA FOR TDEE

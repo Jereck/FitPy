24230dfd6211d07e9f807928a3450749d576e924
Jake <jakereck@gmail.com>
2017-07-01 13:58:30 -0700

    More cleaning up of code
    Had to fix the TDEE and BRM functions

--- fit.py
+++ fit.py
@@ -12,14 +12,17 @@ class Person:
       for key, value in kwargs.items():
          setattr(self, key, value)
 
-   def total_daily_expend(self):
-      #INSERT FORMULA FOR TDEE HERE
+   def basal_metabolic_rate(self):
+      #INSERT FORMULA FOR BMR HERE
       #OUTPUT BASED ON MALE OR FEMALE
       #WOMAN
       return 655 + (9.6 * self.weight) + (1.8 * self.height) - (4.7 * self.age)
       #MALE
       return 66 + (13.7 * self.weight) + (5 * self.height) - (6.8 * self.age)
 
+   def total_daily_expend(self):
+      #INSERT FORMULA FOR TDEE
+      return basal_metabolic_rate() * activity_level
 
 #CREATE THE FUNCTIONS FOR GAIN, MAINTAIN, LOSE
 def gain():
